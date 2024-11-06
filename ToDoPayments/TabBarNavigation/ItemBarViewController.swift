dea534c53cbb7ff293cf881448cf28eedcd0740a
Jesus Garcia <jesus.gg.dev@gmail.com>
2024-11-06 14:36:11 -0600

    Initial configuration and commit

---
+++ ToDoPayments/TabBarNavigation/ItemBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ItemBarViewController.swift
+//  ToDoPayments
+//
+//  Created by Jesus Gonzalez on 06/11/24.
+//
+
+import UIKit
+
+class ItemBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+
+}
---
+++ ToDoPayments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoPayments
+//
+//  Created by Jesus Gonzalez on 06/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
